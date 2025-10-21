4e7d1e80a169e69dc09450f0348905ef6b04b0cc
Raspi-dude <131214288+Raspi-dude@users.noreply.github.com>
2025-10-20 23:27:40 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -27,7 +27,7 @@ porple=$(tput setaf 183)
 reset=$(tput sgr0)
 
 TOS_FILE="DoNotModify"
-TOS_NOTICE="# By editing this file manually, you still agree to the Terms of Service hosted at https://host.raspidude.eu.org/tos"
+TOS_NOTICE="# By editing this file manually, you still agree to the Terms of Service hosted at https://raspihost.org/tos"
 
 motds=(
     "balls"
@@ -67,6 +67,16 @@ motds=(
     "308 Negra Arroyo Lane, Albuquerque, New Mexico 87104"
     "Hard off and Book off are goated"
     "Our next node will run on a Raspberry Pi 0 with a 16gb micro SD card!"
+    "nvm I have a 257 day reddit streak"
+    "does anyone actually read these?"
+    "first person who sends a screenshot of this motd gets a free 314mb storage upgrade (get it? pi? 3.14? hilarious right?)"
+    "Running on 75GXP drives!"
+    "Let's Ishukan Communication!"
+    "|  ||  ||  |_"
+    "But maybe someday when my ship comes in, maybe she'll understand what kind of guy I am"
+    "Check out Serani Poji!"
+    "More More Jump!"
+    "Raspihost cup noodle prohibition"
 )
 
 art=(
@@ -125,6 +135,7 @@ printf "%b%b\n" "${yellow}                     Welcome to Raspi Host! - No BS ho
 printf "%b\n" "${reset} "
 random_motd=${motds[$RANDOM % ${#motds[@]}]}
 printf "%b%b\n" "${mike}Raspi_dude's MOTD: " "${porple}$random_motd"
+printf "%b%b\n" "${yellow}Pro tip: Instead of editing the ServerConfig.toml file, edit the values under the Startup tab"
 
 if [[ ! -f "$TOS_FILE" ]]; then
     echo "$TOS_NOTICE" > "$TOS_FILE"
@@ -135,7 +146,7 @@ fi
 tos_status=$(grep -i "^tos=" "$TOS_FILE" | cut -d '=' -f2 | tr '[:upper:]' '[:lower:]')
 if [[ "$tos_status" != "true" ]]; then
     echo -e "\033[1;31mTOS not accepted.\033[0m"
-    echo "Visit https://host.raspidude.eu.org/tos"
+    echo "Visit https://raspihost.org/tos"
     echo "Do you accept the TOS? (Type in yes/no below): "
 
     read -r user_input
@@ -173,8 +184,8 @@ if [ -z "${VERSION}" ] || [ "${VERSION}" = "latest" ]; then
         | cut -d '"' -f 4)
 else
     echo "Using BeamMP version: ${VERSION}..."
-    DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/BeamMP/BeamMP-Server/releases" \
-        | grep -A20 "\"tag_name\": \"${VERSION}\"" \
+    # Fetch the specific release by tag and parse it properly
+    DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/BeamMP/BeamMP-Server/releases/tags/${VERSION}" \
         | grep "browser_download_url" \
         | grep "$MATCH" \
         | head -n 1 \
@@ -182,7 +193,7 @@ else
 fi
 
 if [ -z "$DOWNLOAD_URL" ]; then
-    echo "Error: Could not find download URL. Please report this to Raspi_dude."
+    echo "Error: Could not find download URL. Please note that versions any older than v3.2.2 will not work."
     exit 1
 fi
 
